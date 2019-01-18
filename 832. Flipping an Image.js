897e20304b8b7ef4d63c92cd28bee53d6131e32d
Nicholas Turner <bindlez@gmail.com>
2019-01-18 02:45:59 -0500

    commit 2019-01-18

---
+++ 832. Flipping an Image.js
@@ -0,0 +1,41 @@
+// 832. Flipping an Image
+image = [[1,1,0],[1,0,1],[0,0,0]]
+//SOL
+function flip(image) {
+  reversed_image = image.map(row => row.reverse());
+  console.log(reversed_image)
+  return reversed_image.map(sub => {
+    return sub.map(num => num === 0 ? 1 : 0)
+  })
+}
+console.log(flip(image))
+
+//WORKING . . . [UNSUCCESS]
+function flip(image) {
+  reversed_image = image.map(row => row.reverse());
+  console.log(reversed_image)
+  reversed_image.map(sub => {
+    sub.map(num => {
+      if (num == 0) {
+        num = 1;
+      } else {
+        num = 0;
+      }
+    })
+  })
+}
+console.log(flip(image))
+
+//MISC
+let str="Hello"
+console.log(Array.from(str).reverse())
+console.log(str.split('').reverse())
+
+
+// def flip_and_invert_image(image)
+//   image.map(&:reverse).map do |ea|
+//   ea.map { |el| el == 0 ? 1 : 0 }
+//   end
+// end
+  
+// p flip_and_invert_image(image)
