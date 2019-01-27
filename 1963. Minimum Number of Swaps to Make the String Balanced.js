8cef73ee0b14cce51538b9ae14127f22db87b2c0
Nicholas Turner <bindlez@gmail.com>
2019-01-27 02:00:47 -0500

    commit 2019-01-27

--- 1963. Minimum Number of Swaps to Make the String Balanced.js
+++ 1963. Minimum Number of Swaps to Make the String Balanced.js
@@ -4,6 +4,7 @@ s = "][]["
  * @param {string} s
  * @return {number}
  */
+//SOL 1 | CORRECTED BY Math.floor()
 var minSwaps = function(s) {
     var open_bracket_count = 0
     Array.from(s).forEach(char => {
@@ -13,7 +14,7 @@ var minSwaps = function(s) {
           open_bracket_count-=1
         }
     })
-    return (open_bracket_count+1) / 2
+    return Math.floor((open_bracket_count+ 1 ) / 2)
 };
 console.log(minSwaps(s))
 
