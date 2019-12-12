2d0c93b677955a0623571ec3be4fefef014be12c
Nicholas Turner <bindlez@gmail.com>
2019-12-12 01:42:42 -0500

    commit 2019-12-12

--- 657. Robot Return to Origin.rs
+++ 657. Robot Return to Origin.rs
@@ -1,6 +1,19 @@
 // 657. Robot Return to Origin
 //moves = "UD"
+//SOL 2
+impl Solution {
+  pub fn judge_circle(moves: String) -> bool {
+      moves.chars().filter(|&c| c == 'U').count() == moves.chars().filter(|&c| c == 'D').count() &&
+      moves.chars().filter(|&c| c == 'L').count() == moves.chars().filter(|&c| c == 'R').count()
+  }
+}
 
+fn main() {
+  let moves = "UDLR".to_string();
+  println!("{}", Solution::judge_circle(moves)); // Output: true
+}
+
+//SOL 1
 impl Solution {
   pub fn judge_circle(moves: String) -> bool {
       let up_count = moves.chars().filter(|&c| c == 'U').count();
