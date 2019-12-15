34b96c5ec2a8be6e5422dc34d230a1dee89b1e34
Nicholas Turner <bindlez@gmail.com>
2019-12-15 00:07:55 -0500

    commit 2019-12-15

---
+++ 657. Robot Return to Origin.cs
@@ -0,0 +1,18 @@
+// 657. Robot Return to Origin
+//moves = "UD"
+using System;
+
+public class Solution {
+    public bool JudgeCircle(string moves) {
+        return moves.Count(c => c == 'U') == moves.Count(c => c == 'D') &&
+               moves.Count(c => c == 'L') == moves.Count(c => c == 'R');
+    }
+}
+
+class Program {
+    static void Main(string[] args) {
+        string moves = "UD";
+        Solution solution = new Solution();
+        Console.WriteLine(solution.JudgeCircle(moves)); // Output: True
+    }
+}
