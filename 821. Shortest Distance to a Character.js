557fba9890849e1c8c1237a3fd0f7a006db55843
Nicholas Turner <bindlez@gmail.com>
2019-12-31 01:21:05 -0500

    commit 2019-12-31

---
+++ 821. Shortest Distance to a Character.js
@@ -0,0 +1,27 @@
+// 821. Shortest Distance to a Character
+s = "loveleetcode"; c = "e"
+function shortestToChar(s, c) {
+  const res = [];
+  const indices = [];
+  
+  for (let i = 0; i < s.length; i++) {
+      if (s[i] === c) {
+          indices.push(i);
+      }
+  }
+  
+  for (let i = 0; i < s.length; i++) {
+      let minDist = Infinity;
+      for (let index of indices) {
+          minDist = Math.min(minDist, Math.abs(i - index));
+      }
+      res.push(minDist);
+  }
+  
+  return res;
+}
+
+// Example usage:
+const s = "loveleetcode";
+const c = "e";
+console.log(shortestToChar(s, c));  // Output: [3, 2, 1, 0, 1, 0, 0, 1, 2, 2, 1, 0]
