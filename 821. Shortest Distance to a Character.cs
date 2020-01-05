da05d1fa88bbe04c42a52fbe267a8e2c02894e82
Nicholas Turner <bindlez@gmail.com>
2020-01-05 00:54:37 -0500

    commit 2020-01-05

---
+++ 821. Shortest Distance to a Character.cs
@@ -0,0 +1,16 @@
+// 821. Shortest Distance to a Character
+//s = "loveleetcode"; c = "e"
+public class Solution {
+    public int[] ShortestToChar(string s, char c) {
+        List<int> ans = new();
+        for (int i = 0; i < s.Length; i++)
+        {
+            int distance = int.MaxValue;
+            for (int j = 0; j < s.Length; j++)
+                if (s[j] == c && distance > Math.Abs(j - i))
+                    distance = Math.Abs(j - i);
+            ans.Add(distance);
+        }
+        return ans.ToArray();
+    }
+}
