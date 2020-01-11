b1c612668146803e7996bd751d0967900eaae52e
Nicholas Turner <bindlez@gmail.com>
2020-01-11 00:12:14 -0500

    commit 2020-01-11

--- 832. Flipping an Image.rb
+++ 832. Flipping an Image.rb
@@ -2,6 +2,12 @@
 image = [[1,1,0],[1,0,1],[0,0,0]]
 # @param {Integer[][]} image
 # @return {Integer[][]}
+#*
+def flip_and_invert_image(image)
+    image.map(&:reverse).map { |row| row.map { |num| 1 - num } }
+end
+p flip_and_invert_image(image)
+#*
 def flip_and_invert_image(image)
     image.map(&:reverse).map do |ea|
         ea.join.gsub('0', '2').gsub('1', '0').gsub('2', '1').chars.map(&:to_i)
@@ -9,7 +15,7 @@ def flip_and_invert_image(image)
 end
 p flip_and_invert_image(image)
 
-
+#*
 def flip_and_invert_image(image)
   image.map(&:reverse).map do |ea|
   ea.map { |el| el == 0 ? 1 : 0 }
