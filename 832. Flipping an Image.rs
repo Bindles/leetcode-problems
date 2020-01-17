d367bba70934e14e20f9ba6f04c03bc3fc84177d
Nicholas Turner <bindlez@gmail.com>
2020-01-17 00:18:23 -0500

    commit 2020-01-17

--- 832. Flipping an Image.rs
+++ 832. Flipping an Image.rs
@@ -8,3 +8,22 @@ impl Solution {
       }).collect()
   }
 }
+
+//SOL LONG
+impl Solution {
+  pub fn flip_and_invert_image(mut image: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
+      for h in 0..image.len() {
+          image[h].reverse();
+      }
+      for i in 0..image.len() {
+          for j in 0..image[0].len() {
+              if image[i][j] == 0 {
+                  image[i][j] = 1;
+              }else {
+                  image[i][j] = 0;
+              }
+          }
+      }
+      image
+  }
+}
