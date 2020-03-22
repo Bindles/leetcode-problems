48657a6bbd6f6f6602d84dc058da73f040e54055
Nicholas Turner <bindlez@gmail.com>
2020-03-22 01:26:59 -0400

    commit 2020-03-22

--- 2710. Remove Trailing Zeros From a String.rb
+++ 2710. Remove Trailing Zeros From a String.rb
@@ -2,7 +2,16 @@
 num = "51230100"
 # @param {String} num
 # @return {String}
+#* LOL GENIUS | WONDER IF IT WORKS IN PY
 def remove_trailing_zeros(num)
   num.reverse.to_i.to_s.reverse
+end
+p remove_trailing_zeros(num)
+
+
+#* INTERESTING OTHER GUYS SOL
+def remove_trailing_zeros(num)
+  num.chomp!('0') while num[-1] == '0'
+  num
 end
 p remove_trailing_zeros(num)
