5f38866164e0cd2558799c537bec2fd47a6a5b84
Nicholas Turner <bindlez@gmail.com>
2020-03-23 00:21:48 -0400

    commit 2020-03-23

--- 2710. Remove Trailing Zeros From a String.js
+++ 2710. Remove Trailing Zeros From a String.js
@@ -1,6 +1,14 @@
 // 2710. Remove Trailing Zeros From a String
 num = "51230100"
+//GOOD LEARNING HERE
+//SOL 2 | SOL 1 DIDINT WORK NEED TO SWAP parseInt => BigInt
+var removeTrailingZeros = function(num) {
+  return BigInt(num.split('').reverse().join('')).toString().split('').reverse().join('');
+};
+
+//WORKING . . .
+//SOL 1 | DOES NOT WORK
 function removeTrailingZeros(num) {
   return parseInt(num.split('').reverse().join('')).toString().split('').reverse().join('');
-}
+};
 console.log(removeTrailingZeros(num));
