5262194dfaa9cbb08903ae91d917085818c3740c
Nicholas Turner <bindlez@gmail.com>
2020-03-24 01:44:27 -0400

    commit 2020-03-24

---
+++ 2710. Remove Trailing Zeros From a String.rs
@@ -0,0 +1,10 @@
+// 2710. Remove Trailing Zeros From a String
+//num = "51230100"
+impl Solution {
+  pub fn remove_trailing_zeros(num: String) -> String {
+      let num_reversed: String = num.chars().rev().collect();
+      let parsed_int = num_reversed.parse::<u64>().unwrap();
+      let result = parsed_int.to_string();
+      result.chars().rev().collect()
+  }
+}
