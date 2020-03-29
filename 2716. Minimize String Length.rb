2446edfc6801d28068af462f86792726baa60170
Nicholas Turner <bindlez@gmail.com>
2020-03-29 00:07:14 -0400

    commit 2020-03-29

--- 2716. Minimize String Length.rb
+++ 2716. Minimize String Length.rb
@@ -7,8 +7,13 @@ s = "aaabc"
 
 # @param {String} s
 # @return {Integer}
-#*
+#* SOL 2 | WHICH IS FASTER BYTES OR CHARS | GUESSING BYTES | TESTING...
 def minimized_string_length(s)
+  s.bytes.tally.size
+end
+p minimized_string_length(s)
+#* SOL 1
+def minimized_string_length(s) 
   s.chars.tally.size
 end
 p minimized_string_length(s)
