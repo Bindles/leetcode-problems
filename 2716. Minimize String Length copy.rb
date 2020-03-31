dc20152f10e0c2be030c117937dd72fc1165daf9
Nicholas Turner <bindlez@gmail.com>
2020-03-31 02:02:12 -0400

    commit 2020-03-31

---
+++ 2716. Minimize String Length copy.rb
@@ -0,0 +1,17 @@
+require 'objspace'
+
+s = "aaaaslkdkldklajdlkasnd,asndandkjasdnadsdlskdlakdlasksfkjhfkdhfsdhfkjdfhjfh dskfdjfh dkfdsfksjdhf dskfhdfhjksfsdfhskjdf kbcs"
+
+chars_array = s.chars
+bytes_array = s.bytes
+
+puts "Memory size of chars_array: #{ObjectSpace.memsize_of(chars_array)}"
+puts "Memory size of bytes_array: #{ObjectSpace.memsize_of(bytes_array)}"
+
+chars_array.each do |char|
+  puts "Memory size of char '#{char}': #{ObjectSpace.memsize_of(char)}"
+end
+
+bytes_array.each do |byte|
+  puts "Memory size of byte #{byte}: #{ObjectSpace.memsize_of(byte)}"
+end
--- 2716. Minimize String Length.rb
+++ 2716. Minimize String Length.rb
@@ -8,6 +8,7 @@ s = "aaabc"
 # @param {String} s
 # @return {Integer}
 #* SOL 2 | WHICH IS FASTER BYTES OR CHARS | GUESSING BYTES | TESTING...
+#* BYTES ARE FASTER 0B VS CHARS 40B | SEE BELOW
 def minimized_string_length(s)
   s.bytes.tally.size
 end
@@ -17,6 +18,20 @@ def minimized_string_length(s)
   s.chars.tally.size
 end
 p minimized_string_length(s)
+
+#* TEST MEMORY
+# Memory size of chars_array: 80
+# Memory size of bytes_array: 80
+# Memory size of char 'a': 40
+# Memory size of char 'a': 40
+# Memory size of char 'b': 40
+# Memory size of char 'c': 40
+# Memory size of char 's': 40
+# Memory size of byte 97: 0
+# Memory size of byte 97: 0
+# Memory size of byte 98: 0
+# Memory size of byte 99: 0
+# Memory size of byte 115: 0
 
 
 #*WORKING. . .
