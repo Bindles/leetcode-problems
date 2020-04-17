a7cdf8dd9f6303724538c5c586a95a77ca9d1703
Nicholas Turner <bindlez@gmail.com>
2020-04-17 01:04:08 -0400

    commit 2020-04-17

--- 1529. Minimum Suffix Flips.py
+++ 1529. Minimum Suffix Flips.py
@@ -3,6 +3,22 @@ target = "10111"
 #*
 class Solution:
     def minFlips(self, target: str) -> int:
+        current_state = "0"
+        count=0
+
+        for char in target:
+            if char != current_state:
+                count += 1
+                current_state = char
+                
+        return count
+
+
+print(Solution().minFlips(target))
+
+#*
+class Solution:
+    def minFlips(self, target: str) -> int:
         n = len(target)
         s =[0] * n
 
