86e6b488e1151d36f76fd11dcbced3ee1f3b9de7
Nicholas Turner <bindlez@gmail.com>
2020-04-18 02:54:54 -0400

    commit 2020-04-18

---
+++ 1529. Minimum Suffix Flips.js
@@ -0,0 +1,18 @@
+// 1529. Minimum Suffix Flips
+target = "10111"
+/**
+ * @param {string} target
+ * @return {number}
+ */
+var minFlips = function(target) {
+  let currentState=0
+  let count=0
+
+  for (char of target) {
+      if (char != currentState) {
+          count++
+          currentState = char
+      }
+  }
+  return count
+};
