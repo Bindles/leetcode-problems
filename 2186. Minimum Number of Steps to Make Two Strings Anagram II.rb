7bf164a8fdcbd775b162a8cbfb29ae3ff71c8bfe
Nicholas Turner <bindlez@gmail.com>
2020-05-01 01:40:20 -0400

    commit 2020-05-01

--- 2186. Minimum Number of Steps to Make Two Strings Anagram II.rb
+++ 2186. Minimum Number of Steps to Make Two Strings Anagram II.rb
@@ -3,6 +3,24 @@ s = "leetcode"; t = "coats"
 # @param {String} s
 # @param {String} t
 # @return {Integer}
+#* SOLVED GOOD PROBLEM LEARNED SOME 
+def min_steps(s, t)
+  s_count = s.chars.tally
+  t_count = t.chars.tally
+  steps = 0
+  # Collect all unique characters from both strings
+  unique_chars = s_count.keys | t_count.keys
+  
+  unique_chars.each do |char|
+    # Calc the diffe in counts of each character
+    s_char_count = s_count[char] || 0
+    t_char_count = t_count[char] || 0
+    steps += (s_char_count - t_char_count).abs
+  end
+  steps
+end
+
+
 #* WORKING . . .
 #*
 def min_steps(s, t)
