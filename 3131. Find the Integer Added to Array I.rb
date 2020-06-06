a9e238eeead4c0f72ed8bb2560be757e7fdae859
Nicholas Turner <bindlez@gmail.com>
2020-06-06 02:14:14 -0400

    commit 2020-06-06

--- 3131. Find the Integer Added to Array I.rb
+++ 3131. Find the Integer Added to Array I.rb
@@ -4,8 +4,9 @@ nums1 = [10]; nums2 = [5]
 # @param {Integer[]} nums1
 # @param {Integer[]} nums2
 # @return {Integer}
+#* SOL 1
 def added_integer(nums1, nums2)
-    p a =nums1.sort.zip(nums2.sort)
-    (a.first.last - a.first.first)
+    x =nums1.sort.zip(nums2.sort)
+    x.first.last - x.first.first
 end
 p added_integer(nums1, nums2)
