da4a116b42cda313885e922b8fd3fd40182db551
Nicholas Turner <bindlez@gmail.com>
2020-06-12 02:12:34 -0400

    commit 2020-06-12

--- 3131. Find the Integer Added to Array I.py
+++ 3131. Find the Integer Added to Array I.py
@@ -1,8 +1,19 @@
 #* 3131. Find the Integer Added to Array I
-#nums1 = [2,6,4]; nums2 = [9,7,5]
-nums1 = [10]; nums2 = [5]
-
+nums1 = [2,6,4]; nums2 = [9,7,5]
+#nums1 = [10]; nums2 = [5]
+from typing import List
+#*
 def addedInteger(nums1, nums2):
-  return min(nums1) - min(nums2)
+  return min(nums2) - min(nums1)
 
 print(addedInteger(nums1, nums2))
+
+#* FOR FUN AND TEST ZIP SORT
+def addedInteger(nums1, nums2):
+  x=list(zip(sorted(nums1), sorted(nums2)))
+  return x[0][1] - x[0][0]
+  
+print(addedInteger(nums1, nums2))
+
+#* MISC
+print(sorted(nums1))
